eb1e9b5d499743fb61f9d310702f5ef8923a8539
Marcos <terceiro.marcos2005@gmail.com>
2025-06-02 14:38:07 -0300

    corrigindo rota de deleção de anexos

--- backend/src/routes/anexosRoutes.js
+++ backend/src/routes/anexosRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const anexoController = require("../controllers/anexoController");
 
 router.post("/obstaculo/:obstaculoId/manobra/:manobraId/anexo", anexoController.adicionarAnexoController);
-router.delete("/obstaculo/:obstaculoId/manobra/:manobraId/anexo/:id", anexoController.removerAnexoController)
+router.delete("/obstaculo/:obstaculoId/manobra/:manobraId/anexo/:anexoId", anexoController.removerAnexoController)
 
 
 module.exports = router;

22da4f175ca20721227319aaa0cc622d4e036bf8
Marcos <terceiro.marcos2005@gmail.com>
2025-06-02 13:56:26 -0300

    ajustando rotas dos enpoints dos anexos

--- backend/src/routes/anexosRoutes.js
+++ backend/src/routes/anexosRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const anexoController = require("../controllers/anexoController");
 
-router.post("/obstaculo/:obstaculoId/manobraId/:id/anexos", anexoController.adicionarAnexoController);
-router.delete("/obstaculo/:obstaculoId/manobraId/:id/anexos/:id", anexoController.removerAnexoController)
+router.post("/obstaculo/:obstaculoId/manobra/:manobraId/anexos", anexoController.adicionarAnexoController);
+router.delete("/obstaculo/:obstaculoId/manobra/:manobraId/anexos/:id", anexoController.removerAnexoController)
 
 
 module.exports = router;

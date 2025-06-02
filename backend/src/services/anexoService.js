391c776632c18bb8e3f69f64a2a4db0c580b83aa
Marcos <terceiro.marcos2005@gmail.com>
2025-06-02 14:55:48 -0300

    refatorando o service de deletar anexo

--- backend/src/services/anexoService.js
+++ backend/src/services/anexoService.js
@@ -25,25 +25,34 @@ const adicionarAnexoService = async (obstaculoId, manobraId, novoAnexo) => {
 };
 
 const removerAnexoService = async (obstaculoId, manobraId, anexoId) => {
-  console.log("Service - ", obstaculoId, manobraId, anexoId);
-
   try {
-    const obstaculo = await Obstaculo.findOneAndUpdate(
-      {
-        _id: obstaculoId,
-        "manobras._id": manobraId,
-      },
-      {
-        $pull: { "manobras.$.anexos": { _id: anexoId } },
-      },
-      { new: true }
-    );
+    const obstaculo = await Obstaculo.findOne({
+      _id: obstaculoId,
+      "manobras._id": manobraId,
+    });
 
     if (!obstaculo) {
-      throw new Error("Obstáculo, manobra ou anexo não encontrado");
+      throw new Error("Obstáculo ou manobra não encontrado");
     }
 
-    return obstaculo;
+    const manobra = obstaculo.manobras.id(manobraId);
+    if (!manobra) {
+      throw new Error("Manobra não encontrada");
+    }
+
+    const anexo = manobra.anexos.id(anexoId);
+    if (!anexo) {
+      throw new Error("Anexo não encontrado");
+    }
+
+    anexo.remove();
+
+    await obstaculo.save();
+
+    return {
+      message: "Anexo removido com sucesso",
+      obstaculoAtualizado: obstaculo,
+    };
   } catch (error) {
     throw error;
   }
